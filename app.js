a429df47ce6bc3c3931ca1d6e3e33c2d8be3dd36
romanakhatun <romanakhatun2004@gmail.com>
2025-09-15 20:03:28 +0600

    devfinder start

---
+++ app.js
@@ -0,0 +1,17 @@
+const toggle = document.querySelector(".theme-controller");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.classList.add("dark");
+  toggle.checked = true; // checkbox sync
+}
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    //documentElement => represent root <html> element
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
+});
